c8f669c4b19db7e51e99341695246aaca263c440
Opu Das <95124103+dcpopu@users.noreply.github.com>
2022-09-20 13:16:32 +0600

    add new dto

---
+++ ERPBO/Agriculture/DomainModels/RawMaterialIssueStockInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPBO.Agriculture.DomainModels
+{
+    [Table("tblRawMaterialIssueStockInfo")]
+    public class RawMaterialIssueStockInfo
+    {
+        [Key]
+        public long RawMaterialIssueStockId { get; set; }
+        public string ProductBatchCode { get; set; }
+        public long OrganizationId { get; set; }
+        public long RawMaterialId { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public long? EntryUserId { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public long? UpdateUserId { get; set; }
+        public ICollection<RawMaterialStockDetail> RawMaterialStockDetails { get; set; }
+    }
+}
