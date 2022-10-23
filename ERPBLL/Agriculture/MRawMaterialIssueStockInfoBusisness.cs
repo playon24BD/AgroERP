105de66518f8018d4b1d0f2a470d42812835e9b4
MD Ileas Sha Emon <emonshah789@gmail.com>
2022-10-23 11:43:45 +0600

    up

--- ERPBLL/Agriculture/MRawMaterialIssueStockInfoBusisness.cs
+++ ERPBLL/Agriculture/MRawMaterialIssueStockInfoBusisness.cs
@@ -1,4 +1,6 @@
-﻿ using ERPBLL.Agriculture.Interface;
+﻿using ERPBLL.Agriculture.Interface;
+using ERPBO.Agriculture.DomainModels;
+using ERPBO.Agriculture.DTOModels;
 using ERPDAL.AgricultureDAL;
 using System;
 using System.Collections.Generic;
@@ -12,6 +14,7 @@ namespace ERPBLL.Agriculture
     {
         private readonly IAgricultureUnitOfWork _agricultureUnitOfWork;
         private readonly MRawMaterialIssueStockInfoRepository _mRawMaterialIssueStockInfoRepository;
+        private readonly RawMaterialTrackInfoRepository _rawMaterialTrackInfoRepository;
 
 
         //contractor
@@ -19,9 +22,74 @@ namespace ERPBLL.Agriculture
         {
             this._agricultureUnitOfWork = agricultureUnitOfWork;
             this._mRawMaterialIssueStockInfoRepository = new MRawMaterialIssueStockInfoRepository(this._agricultureUnitOfWork);
+            this._rawMaterialTrackInfoRepository = new RawMaterialTrackInfoRepository(this._agricultureUnitOfWork);
         }
 
+        public bool SaveRawMaterialIssueStock(MRawMaterialIssueStockInfoDTO info, List<MRawMaterialIssueStockDetailsDTO> details, long userId, long orgId)
+        {
+            bool IsSuccess = false;
+            var BatchCode = "BPIS-" + DateTime.Now.ToString("MM") + DateTime.Now.ToString("yy") + DateTime.Now.ToString("dd") + DateTime.Now.ToString("hh") + DateTime.Now.ToString("mm") + DateTime.Now.ToString("ss");
 
 
+            if (info.RawMaterialIssueStockId == 0)
+            {
+                MRawMaterialIssueStockInfo model = new MRawMaterialIssueStockInfo
+                {
+                    ProductBatchCode = BatchCode,
+                    EntryDate = DateTime.Now,
+                    EntryUserId = userId,
+                    OrganizationId =orgId,
+                    Status = "Active",
+
+                };
+                List<MRawMaterialIssueStockDetails> modelDetails = new List<MRawMaterialIssueStockDetails>();
+                List<RawMaterialTrack> modeltrk = new List<RawMaterialTrack>();//RawMaterialTrackInfo table update
+
+                foreach (var item in details)
+                {
+                    MRawMaterialIssueStockDetails materialIssueStockDetails = new MRawMaterialIssueStockDetails()
+                    {
+
+                        RawMaterialId = item.RawMaterialId,
+                        Quantity = item.Quantity,
+                        UnitID = item.UnitID,
+                        IssueStatus = "StockOut",
+                        EntryDate = DateTime.Now,
+
+                    };
+                    modelDetails.Add(materialIssueStockDetails);
+
+
+                    //RawMaterialTrackInfo table update
+                    RawMaterialTrack rawMaterialTrack = new RawMaterialTrack()
+                    {
+                        RawMaterialId = item.RawMaterialId,
+                        Quantity = item.Quantity,
+                        IssueDate = DateTime.Now,
+                        EntryDate = DateTime.Now,
+                        IssueStatus = "StockOut",
+                        EntryUserId = userId
+                    };
+                    modeltrk.Add(rawMaterialTrack);
+
+                }
+
+                model.MRawMaterialIssueStockDetails = modelDetails;
+
+                _mRawMaterialIssueStockInfoRepository.Insert(model);
+                IsSuccess = _mRawMaterialIssueStockInfoRepository.Save();
+
+                _rawMaterialTrackInfoRepository.InsertAll(modeltrk);
+                IsSuccess = _rawMaterialTrackInfoRepository.Save();
+
+            }
+            //else
+            //{
+
+            //    IsSuccess = _fDetail.updateFinishGoodRecipDetails(info, details, userId, orgId);
+
+            //}
+            return IsSuccess;
+        }
     }
 }
---
+++ ERPDAL/AgricultureContextMigrations/202210230541244_Rawm issue details tableup.cs
@@ -0,0 +1,39 @@
+namespace ERPDAL.AgricultureContextMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Rawmissuedetailstableup : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tblMRawMaterialIssueStockInfo", "Status", c => c.String());
+            AlterColumn("dbo.tblMRawMaterialIssueStockDetails", "IssueStatus", c => c.String());
+            DropColumn("dbo.tblMRawMaterialIssueStockInfo", "IssueStatus");
+            DropTable("dbo.DivisionUsers");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.DivisionUsers",
+                c => new
+                    {
+                        DivisionUserId = c.Long(nullable: false, identity: true),
+                        UserId = c.Long(nullable: false),
+                        DivisionId = c.Long(nullable: false),
+                        EntryDate = c.DateTime(),
+                        EntryUserId = c.Long(),
+                        UpdateDate = c.DateTime(),
+                        UpdateUserId = c.Long(),
+                        Status = c.String(),
+                        OrganizationId = c.Long(nullable: false),
+                    })
+                .PrimaryKey(t => t.DivisionUserId);
+            
+            AddColumn("dbo.tblMRawMaterialIssueStockInfo", "IssueStatus", c => c.Long(nullable: false));
+            AlterColumn("dbo.tblMRawMaterialIssueStockDetails", "IssueStatus", c => c.Long(nullable: false));
+            DropColumn("dbo.tblMRawMaterialIssueStockInfo", "Status");
+        }
+    }
+}
