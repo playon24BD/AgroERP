ce332d4b4dc29945a31ceec5555160db703d7dc1
AroshP <aroshp24@gmail.com>
2022-10-23 11:15:49 +0600

    DivisionUser

---
+++ ERPBO/Agriculture/DTOModels/DivisionUserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERPBO.Agriculture.DTOModels
+{
+   public class DivisionUserDTO
+    {
+        public long UserId { get; set; }
+        public long DivisionId { get; set; }
+        public DateTime? EntryDate { get; set; }
+        public long? EntryUserId { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public long? UpdateUserId { get; set; }
+        public string Status { get; set; }
+    }
+}
