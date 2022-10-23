8ff82be4e856e886579f56d48a157f5656241547
AroshP <aroshp24@gmail.com>
2022-10-23 14:00:52 +0600

    div

--- ERPBLL/Agriculture/Interface/IDivisionUserBusiness.cs
+++ ERPBLL/Agriculture/Interface/IDivisionUserBusiness.cs
@@ -9,7 +9,7 @@ namespace ERPBLL.Agriculture.Interface
 {
    public interface IDivisionUserBusiness
     {
-        bool SaveDivisionsUser(List<long> divisions, long userId, long suserId, long orgId);
+        bool SaveDivisionsUser(List<string> divisions, long userId, long suserId, long orgId);
         DivisionUser GetAllDivisionsByUserIdAndDivisionId(long userId, long divisionId, long orgId);
         bool UpdateDivisions(List<long> divisions, long userId, long suserId, long orgId);
     }
